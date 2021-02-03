ca7904e5ee5c3b19919572231f0dfe603de0c654
shreyabkatte <57622860+shreyabkatte@users.noreply.github.com>
2021-02-04 02:47:50 +0530

    Circle.js update

--- src/icons/Circle.js
+++ src/icons/Circle.js
@@ -1,24 +1,12 @@
-const CircleIcon = ({ width, height, fill }) => {
+const CircleIcon = ({ width, height, fill="none" }) => {
     return (
-        <svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="25" height="25" viewBox="0 0 37 37" fill={fill} xmlns="http://www.w3.org/2000/svg">
             <circle cx="18.5" cy="18.5" r="17.5" stroke="#F2F2F2" stroke-width="2" />
         </svg>
     )
 }
 
 export default CircleIcon
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
